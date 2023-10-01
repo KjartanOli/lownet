e276d7deb15aabf1824121a828f116bd318baaad
Kjartan Oli Agustsson <kjartanoli@disroot.org>
2023-10-01 13:09:40 +0000

    Implement chat
    * src/main/app_chat.c (chat_receive, chat_shout, chat_tell):
      Implement.
      (chat_valid_message): New function.

--- src/main/app_chat.c
+++ src/main/app_chat.c
@@ -9,6 +9,7 @@
 
 #include "lownet.h"
 #include "serial_io.h"
+#include "utility.h"
 
 #include "app_chat.h"
 
@@ -16,18 +17,64 @@ void chat_receive(const lownet_frame_t* frame) {
 	if (frame->destination == lownet_get_device_id()) {
 		// This is a tell message, just for us!
 
-		//  ...?
+		// id + " says: " + null terminator
+		char buffer[4 + 7 + 1];
+		sprintf(buffer, "0x%x says: ", frame->source);
+		serial_write_line(buffer);
+		serial_write_line((char*) frame->payload);
 	} else {
-		// This is a broadcast shout message.
-
-		//	...?
+		// id + " shouts: " + null terminator
+		char buffer[4 + 9 + 1];
+		sprintf(buffer, "0x%x shouts: ", frame->source);
+		serial_write_line(buffer);
+		serial_write_line((char*) frame->payload);
 	}
 }
 
+// Usage: chat_valid_message(MESSAGE)
+// Pre:   MESSAGE != NULL
+// Value: true if MESSAGE is a valid message, false otherwise
+bool chat_valid_message(const char* message)
+{
+	size_t length = strlen(message);
+	if (length > LOWNET_PAYLOAD_SIZE)
+		return false;
+
+	for (int i = 0; message[i]; i++)
+		{
+			if (!util_printable(message[i]))
+				return false;
+		}
+
+	return true;
+}
+
 void chat_shout(const char* message) {
-	// ...?
+	if (!(message || chat_valid_message(message)))
+		return;
+
+	size_t length = strlen(message);
+
+	lownet_frame_t frame;
+	frame.source = lownet_get_device_id();
+	frame.destination = 0xFF;
+	frame.protocol = LOWNET_PROTOCOL_CHAT;
+	frame.length = length;
+	memcpy(&frame.payload, message, length);
+	lownet_send(&frame);
 }
 
 void chat_tell(const char* message, uint8_t destination) {
-	// ...?
+		if (!(message || chat_valid_message(message)))
+			return;
+
+	size_t length = strlen(message);
+
+	lownet_frame_t frame;
+	frame.source = lownet_get_device_id();
+	frame.destination = destination;
+	frame.protocol = LOWNET_PROTOCOL_CHAT;
+	frame.length = length;
+	memcpy(&frame.payload, message, length);
+	lownet_send(&frame);
 }
