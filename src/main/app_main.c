424571c15eaa877a53230ff1e1e2ec356d4ee6eb
Kjartan Oli Agustsson <kjartanoli@disroot.org>
2023-10-01 13:22:15 +0000

    Implement command dispatch
    * src/app/main/app_main.c (date, ctoi, parse_id): New function.
    (app_main): Parse commands and arguments and dispatch to appropriate
    functions.

--- src/main/app_main.c
+++ src/main/app_main.c
@@ -29,11 +29,79 @@ void app_frame_dispatch(const lownet_frame_t* frame) {
 	}
 }
 
+void date()
+{
+	lownet_time_t time = lownet_get_time();
+	if (time.seconds == 0 && time.parts == 0)
+		{
+			serial_write_line("Network time is not available.\n");
+			return;
+		}
+
+	// seconds + period + parts + description
+	char buffer[11 + 1 + 3 + 32];
+	sprintf(buffer, "%lu.%u sec since the course started.\n", time.seconds, time.parts);
+	serial_write_line(buffer);
+}
+
+// Usage: ctoi(C)
+// Pre:   C is a valid hexadecimal character
+// Value: The decimal value of C as a hexadecimal character or 255 if
+//        C is not a valid hexadecimal character
+uint8_t ctoi(char c)
+{
+	if ('0' <= c && c <= '9')
+		return c - '0';
+	else if ('a' <= c && c <= 'f')
+		return 10 + c - 'a';
+	else if ('A' <= c && c <= 'F')
+		return 10 + c - 'A';
+	else
+		return 255;
+}
+
+// Usage: parse_id(STR, DEST)
+// Pre:   STR != NULL,
+//        3 <= strlen(STR) <= 4,
+//        STR is a valid hexadecimal number,
+//        DEST != NULL
+// Value: true if STR was successfully parsed into a valid id
+// Post: The value pointed to by DEST has been set to the id parsed
+//       from STR
+bool parse_id(char* str, uint8_t* dest)
+{
+	if (!str)
+		return false;
+
+	size_t length = strlen(str);
+	if (length < 3 || length > 4 || str[0] != '0' || str[1] != 'x')
+		return false;
+
+	if (length == 3)
+		{
+			uint8_t t = ctoi(str[2]);
+			if (t == 255)
+				return false;
+			*dest = t;
+		}
+
+	uint8_t t = ctoi(str[2]);
+	if (t == 255)
+		return false;
+	*dest = t * 16;
+	t = ctoi(str[3]);
+	if (t == 255)
+		return false;
+	*dest += t;
+
+	return true;
+}
+
 void app_main(void)
 {
 	char msg_in[MSG_BUFFER_LENGTH];
 	char msg_out[MSG_BUFFER_LENGTH];
-	
+
 	// Initialize the serial services.
 	init_serial_service();
 
@@ -47,15 +115,48 @@ void app_main(void)
 		if (!serial_read_line(msg_in)) {
 			// Quick & dirty input parse.  Presume input length > 0.
 			if (msg_in[0] == '/') {
-				// Some kind of command.
-				//	...?
+				char* command = strtok(msg_in + 1, " ");
+				char* argument = strtok(NULL, " ");
+
+				if (!strcmp(command, "ping"))
+					{
+						if (!argument)
+							{
+								serial_write_line("A node id must be provided\n");
+								continue;
+							}
+
+						uint8_t dest;
+						if (!parse_id(argument, &dest))
+							{
+								serial_write_line("Invalid node id\n");
+								continue;
+							}
+
+						ping(dest);
+					}
+				else if (!strcmp(command, "date"))
+					date();
 			} else if (msg_in[0] == '@') {
-				// Probably a chat 'tell' command.
-				//	...?
-				chat_tell(/* ...? */);
+				char* dest = strtok(msg_in + 1, " ");
+				char* message = strtok(NULL, "\n");
+
+				uint8_t d;
+				if (!parse_id(dest, &d))
+					{
+						serial_write_line("Invalid node id\n");
+						continue;
+					}
+
+				if (!message)
+					{
+						serial_write_line("A message must be provided\n");
+						continue;
+					}
+
+				chat_tell(message, d);
 			} else {
 				// Default, chat broadcast message.
-				//	...?
 				chat_shout(msg_in);
 			}
 		}
