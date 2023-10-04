0d930140c992016f930b7789d7aefebd8930cf19
Kjartan Oli Agustsson <kjartanoli@disroot.org>
2023-10-04 17:06:03 +0000

    Distinguish between pings and pongs
    * src/main/app_ping.c (ping_receive): Check frame->origin to
      distinguish between pings from others and replies to our pings.

--- src/main/app_ping.c
+++ src/main/app_ping.c
@@ -38,11 +38,25 @@ void ping_receive(const lownet_frame_t* frame) {
 		// Not intended for us
 		return;
 
-	lownet_frame_t reply;
-	reply.source = lownet_get_device_id();
-	reply.destination = frame->source;
-	reply.protocol = frame->protocol;
-	memcpy(&reply.payload, frame->payload, sizeof(ping_packet_t));
-	((ping_packet_t*) &reply.payload)->timestamp_back = lownet_get_time();
-	lownet_send(&reply);
+	if (((ping_packet_t*) frame->payload)->origin == lownet_get_device_id())
+		{
+			ping_packet_t* packet = ((ping_packet_t*) frame->payload);
+			lownet_time_t rtt;
+			rtt.seconds = packet->timestamp_back.seconds - packet->timestamp_out.seconds;
+			rtt.parts = packet->timestamp_back.parts - packet->timestamp_out.parts;
+
+			// reply from + id + rtt: + seconds + unit + part + suffix
+			char buffer[12 + 4 + 5 + 11 + 2 + 3 + 4];
+			sprintf(buffer, "Reply from: 0x%x RTT: %lus %u/256", frame->source, rtt.seconds, rtt.parts);
+			serial_write_line(buffer);
+		}
+	else {
+		lownet_frame_t reply;
+		reply.source = lownet_get_device_id();
+		reply.destination = frame->source;
+		reply.protocol = frame->protocol;
+		memcpy(&reply.payload, frame->payload, sizeof(ping_packet_t));
+		((ping_packet_t*) &reply.payload)->timestamp_back = lownet_get_time();
+		lownet_send(&reply);
+	}
 }
