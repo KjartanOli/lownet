cb648ef199304254ad783634c49a7a72c0f566e1
Kjartan Oli Agustsson <kjartanoli@disroot.org>
2023-10-25 16:16:27 +0000

    Add documentation for crypt_command
    * src/components/crypt/crypt.h (crypt_command): Add documentation.

--- src/components/crypt/crypt.h
+++ src/components/crypt/crypt.h
@@ -6,5 +6,12 @@
 void crypt_decrypt(const lownet_secure_frame_t* cipher, lownet_secure_frame_t* plain);
 void crypt_encrypt(const lownet_secure_frame_t* plain, lownet_secure_frame_t* cipher);
 
+// Usage: crypt_command(KEY)
+// Pre:   KEY is 0, 1, or a AES key
+// Post:  If key was 0 or 1 the corresponding predefined key has been
+//        set as active.  Otherwise KEY has been set as the active key.
+// Note:  If key is shorter than LOWNET_KEY_SIZE_AES it will be padded
+//        with zeroes.
 void crypt_command(char* args);
+
 #endif
