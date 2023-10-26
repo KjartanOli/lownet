51efd1108d3cf5818e9f3ef87c05f0985c4b8755
Kjartan Oli Agustsson <kjartanoli@disroot.org>
2023-10-26 18:14:48 +0000

    Use structures from David
    * src/components/command/command.c (cmd_packet_t): Rename from
      command_t.
      (cmd_signature_t): New struct.
      (command_receive): Adjust to new name of command_t.
      (CMD_HASH_SIZE, CMD_BLOCK_SIZE): New constants.

--- src/components/command/command.c
+++ src/components/command/command.c
@@ -3,6 +3,9 @@
 #include <stdio.h>
 #include "serial_io.h"
 
+#define CMD_HASH_SIZE 32
+#define CMD_BLOCK_SIZE 256
+
 #define FRAME_TYPE_UNSIGNED 0b00
 #define FRAME_TYPE_SIGNED 0b01
 #define FRAME_TYPE_SIG1 0b10
@@ -19,10 +22,17 @@ uint8_t get_frame_type(const lownet_frame_t* frame)
 typedef struct __attribute__((__packed__))
 {
 	uint64_t sequence;
-	uint8_t command;
+	uint8_t type;
 	uint8_t reserved[3];
-	char data[180];
-} command_t;
+	uint8_t contents[180];
+} cmd_packet_t;
+
+typedef struct __attribute__((__packed__))
+{
+	uint8_t hash_key[CMD_HASH_SIZE];
+	uint8_t hash_msg[CMD_HASH_SIZE];
+	uint8_t sig_part[CMD_BLOCK_SIZE / 2];
+} cmd_signature_t;
 
 typedef enum
 {
@@ -33,13 +43,14 @@ typedef enum
 
 struct {
 	state_t state;
+	uint64_t last_valid;
 } state;
 
 void command_receive(const lownet_frame_t* frame)
 {
-	const command_t* command = (const command_t*) &frame->payload;
+	const cmd_packet_t* command = (const cmd_packet_t*) &frame->payload;
 
 	char buffer[255];
-	sprintf(buffer, "Command {\n\tType: %d\n\tSequence: %llx,\n\tCommand: %d\n}\n", get_frame_type(frame), command->sequence, command->command);
+	sprintf(buffer, "Command {\n\tType: %d\n\tSequence: %llx,\n\tCommand: %d\n}\n", get_frame_type(frame), command->sequence, command->type);
 	serial_write_line(buffer);
 }
