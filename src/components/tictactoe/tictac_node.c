942634bd064492a0ebbda241bfb30efc9f07bf1b
Kjartan Oli Agustsson <kjartanoli@disroot.org>
2023-11-22 18:48:46 +0000

    Add a 'attention span'
    * src/components/tictactoe/tictac_node.c (point_t): New type.
      (attention_field_t): New type.
    
      (left_bound, right_bound, upper_bound, lower_bound, field_squares,
      extend_left, extend_right, extend_up, extend_down,
      field_expand_maybe, init_attention_field): New functions.
    
    The game tree resulting from the entire board is significantly to
    large to even attempt to process.  Thankfully we can expect most moves
    to cluster together, so we only need to pay attention to a small
    section of the board.

--- src/components/tictactoe/tictac_node.c
+++ src/components/tictactoe/tictac_node.c
@@ -51,11 +51,66 @@ uint32_t tictac_checksum(const tictactoe_t* board)
  * - Specify the move by updating *x and *y (pointers)
  * - Return value 0 on making a succesful move (always!)
  */
-int tictac_move(const tictactoe_t* board, int* xp, int* yp, uint8_t s, uint32_t time_ms)
+typedef struct
 {
-	/*
-	 * TODO: figure out a better move!
-	 */
+	uint8_t x;
+	uint8_t y;
+} point_t;
+
+typedef struct
+{
+	point_t top_left;
+	uint8_t width;
+	uint8_t height;
+} attention_field_t;
+
+uint8_t left_bound(const attention_field_t* field)
+{
+	return field->top_left.x;
+}
+
+uint8_t right_bound(const attention_field_t* field)
+{
+	return left_bound(field) + field->width;
+}
+
+uint8_t upper_bound(const attention_field_t* field)
+{
+	return field->top_left.y;
+}
+
+uint8_t lower_bound(const attention_field_t* field)
+{
+	return upper_bound(field) + field->height;
+}
+
+size_t field_squares(const attention_field_t* field)
+{
+	return (right_bound(field) - left_bound(field)) * (lower_bound(field) - upper_bound(field));
+}
+
+void extend_left(attention_field_t* field)
+{
+	field->top_left.x -= 1;
+	field->width += 1;
+}
+
+void extend_right(attention_field_t* field)
+{
+	field->width += 1;
+}
+
+void extend_up(attention_field_t* field)
+{
+	field->top_left.y -= 1;
+	field->height += 1;
+}
+
+void extend_down(attention_field_t* field)
+{
+	field->height += 1;
+}
+
 // Usage: square_free(BOARD, X, Y)
 // Pre:   0 <= X < TICTACTOE_BOARD
 //        0 <= Y < TICTACTOE_BOARD
@@ -65,6 +120,49 @@ bool square_free(const tictactoe_t* board, uint8_t x, uint8_t y)
 	return tictac_get(board, x, y) == EMPTY;
 }
 
+void field_expand_maybe(const tictactoe_t* board, attention_field_t* attention)
+{
+	size_t free_squares = 0;
+	for (uint8_t i = upper_bound(attention), l = lower_bound(attention); i < l; ++i)
+		for (uint8_t j = left_bound(attention), r = right_bound(attention); j < r; ++j)
+			if (square_free(board, i, j))
+				++free_squares;
 
+	if ((float) free_squares / field_squares(attention) < 0.5)
+		{
+			if (left_bound(attention) != 0)
+				extend_left(attention);
+			if (right_bound(attention) != TICTACTOE_BOARD)
+				extend_right(attention);
+			if (upper_bound(attention) != 0)
+				extend_up(attention);
+			if (lower_bound(attention) != TICTACTOE_BOARD)
+				extend_down(attention);
+		}
+}
+
+void init_attention_field(const tictactoe_t* board, attention_field_t* attention)
+{
+	attention->width = 8;
+	attention->height = 8;
+	for (uint8_t i = 0; i < TICTACTOE_BOARD; ++i)
+		{
+			for (uint8_t j = 0; j < TICTACTOE_BOARD; ++j)
+				{
+					if (!square_free(board, i, j))
+						{
+							attention->top_left.x = (j == 0) ? j : j - 1;
+							attention->top_left.y = (i == 0) ? i : i - 1;
+							return;
+						}
+				}
+		}
+}
+
+int tictac_move(const tictactoe_t* board, int* xp, int* yp, uint8_t s, uint32_t time_ms)
+{
+	/*
+	 * TODO: figure out a better move!
+	 */
 	return tictac_auto(board, xp, yp, s);
 }
